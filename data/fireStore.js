a2a337e0d3ddac0e48c7a75056151211b8cea18c
sfdjsh <sfdjsh@naver.com>
2025-02-10 21:56:12 +0900

    env 수정

--- data/fireStore.js
+++ data/fireStore.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
-  measurementId: process.env.MESSAGING_SENDER_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
